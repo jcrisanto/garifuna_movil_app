b25b2f8baea3b4f65e42ca3fc976dea9ffdd6bec
Jorge Crisanto <jcrisanto0@gmail.com>
2020-01-14 22:35:55 -0600

    this my first commit

---
+++ android/app/src/main/kotlin/com/example/garifuna_movil_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.garifuna_movil_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
